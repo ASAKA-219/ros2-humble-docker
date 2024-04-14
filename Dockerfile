e4f9b09f69b2ff748a40ddbe4818bcba809594bb
Yusuke Asaka <yusuke0219asaka@gmail.com>
2024-04-14 22:13:11 +0900

    gpu付きコンテナを追加

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:22.04
+FROM nvidia/cuda:12.1.0-runtime-ubuntu22.04
 SHELL ["/bin/bash", "-c"]
 
 # Timezone, Launguage設定
---
+++ entry_exec.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# イメージが存在するかどうかを確認
+image_name="humble"
+container="humble-test"
+option=$1
+
+if [[ $option == "-gpu" ]]; then
+    container="${container}-gpu"
+    image_name="${image_name}:gpu"
+else
+    image_name="humble:cpu"
+fi
+
+image_exists=$(docker images -q "${image_name}")
+
+# 条件分岐でイメージが存在するかを判断
+if [ -n "$image_exists" ]; then
+  # イメージが存在する場合の処理
+  echo "イメージ"${image_name}"を見つけたよ！"${image_name}"を実行するね！"
+  #docker compose up -d $container
+else
+  # イメージが存在しない場合の処理
+  echo "イメージ" ${image_name} "は見つかりませんでした。別の方法でコンテナを起動するよ！"
+  docker compose up -d --build $container
+fi
+docker compose exec $container /bin/bash
